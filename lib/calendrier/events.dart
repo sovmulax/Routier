1a8d36f02e8916fd82236b050fb091e76ddeb1c7
evaneX0X0 <soumailaevane@gmail.com>
2021-12-06 02:39:15 +0000

    config, application de base mis en place

---
+++ lib/calendrier/events.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:routier/actualit%C3%A9/fil.dart';
+import 'package:routier/annuaire.dart';
+import 'package:routier/forum.dart';
+
+void main() {
+  runApp(Event());
+}
+
+class Event extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Event',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'CALENDRIER'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          Container(
+            child: Image.asset('assets/images/logo.png', width: 50,),
+            margin: EdgeInsets.only(right: 20),
+          )
+        ],
+        title: Text(
+          title,
+          textAlign: TextAlign.center,
+        ),
+
+        backgroundColor: Color.fromRGBO(21, 106, 155, 1),
+      ),
+      body: const Center(
+        child: Text('Contenu de la page'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(21, 106, 155, 1),
+              ),
+              child: Text('Header'),
+            ),
+            ListTile(
+              title: const Text('Actualité'),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => Fil()));
+              },
+            ),
+            ListTile(
+              title: const Text('Calendrier Routier'),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => Event()));
+              },
+            ),
+            ListTile(
+              title: const Text('Forum de Discussion'),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => Forum()));
+              },
+            ),
+            ListTile(
+              title: const Text('Annuaire de Contact'),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => Annuaire()));
+              },
+            ),
+            ListTile(
+              title: const Text('Déconnexion'),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => Fil()));
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,72 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:routier/menu.dart';
+import 'actualité/fil.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.teal,
+        ),
+        home: SplashScreen());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
+//My splash screen class
+class SplashScreen extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<StatefulWidget> createState() {
+    return StateScreen();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+//Managing the states with a new class: StateScreen
+class StateScreen extends State<SplashScreen> {
+  //Definition of the content of the splash screen
+  Widget mySplash =
+      Column(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+    Container(
+      child: Image.asset('assets/images/logo.png', width: 250, height: 250),
+    ),
+    Padding(
+      padding: EdgeInsets.only(top: 40),
+    ),
+  ]);
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  //Overriding the constructor
+  @override
+  void initState() {
+    super.initState();
+    startTimer();
   }
 
+  startTimer() async {
+    var duration = Duration(
+        seconds: 3); //We set the duration of the splash screen at 5 seconds
+    return Timer(duration, route);
+  }
+
+  //Method allowing us to access the afterSplash page
+  route() {
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => Fil()));
+  }
+
+  //Overriding the build function of StateScreen
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+        backgroundColor: Color.fromRGBO(21, 106, 155, 1),
+        body: Center(child: mySplash) //SplashScreen()
+        );
   }
 }

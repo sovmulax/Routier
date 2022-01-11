b03e330fcb82f0268470fdd733d86e9220fb00b3
iam_Sangohan <mensahluc81@gmail.com>
2022-01-11 11:29:23 +0000

    A

---
+++ lib/forum/forum.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:routier/menu.dart';
+
+void main() {
+  runApp(const Forum());
+}
+
+class Forum extends StatelessWidget {
+  const Forum({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Forum',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'FORUM'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          actions: [
+            Container(
+              child: Image.asset(
+                'assets/images/logo.png',
+                width: 50,
+              ),
+              margin: const EdgeInsets.only(right: 20),
+            )
+          ],
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+          ),
+          backgroundColor: const Color.fromRGBO(21, 106, 155, 1),
+        ),
+        body: Center(
+          child: Row(
+            children: const [
+              Center(
+                child: Text(
+                  "COMMUNE",
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(
+            Icons.create,
+          ),
+          backgroundColor: const Color.fromRGBO(21, 106, 155, 1),
+        ),
+        drawer: const SideMenu());
+  }
+}

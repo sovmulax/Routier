78e2b9e1adeadfef2129b4d58656c119b0b4137e
iam_Sangohan <mensahluc81@gmail.com>
2022-01-14 12:17:40 +0000

    Message du forum (en cours)

--- lib/forum/forum.dart
+++ lib/forum/forum.dart
@@ -59,18 +59,18 @@ class MyHomePage extends StatelessWidget {
                 height: 10,
               ),
               Expanded(
-                child: MessageSection(),
+                child: ChatPage(),
               ),
             ],
           ),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: const Icon(
-            Icons.create,
-          ),
-          backgroundColor: const Color.fromRGBO(21, 106, 155, 1),
-        ),
+        // floatingActionButton: FloatingActionButton(
+        //   onPressed: () {},
+        //   child: const Icon(
+        //     Icons.create,
+        //   ),
+        //   backgroundColor: const Color.fromRGBO(21, 106, 155, 1),
+        // ),
         drawer: const SideMenu());
   }
 }
@@ -178,6 +178,7 @@ class MessageSection extends StatelessWidget {
                                 ),
                               ),
                               Wrap(
+                                direction: Axis.horizontal,
                                 children: [
                                   Text(
                                     messages['message'],
@@ -209,6 +210,424 @@ class MessageSection extends StatelessWidget {
           );
         }).toList(),
       ),
+    );
+  }
+}
+
+class ChatPage extends StatelessWidget {
+  const ChatPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.black,
+      bottomNavigationBar: BottomSection(),
+      body: ChatingSection(),
+    );
+  }
+}
+
+class BottomSection extends StatelessWidget {
+  const BottomSection({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      elevation: 10,
+      child: Container(
+        //height: 80,
+        padding: EdgeInsets.all(20),
+        //color: const Color.fromRGBO(21, 106, 155, 1),
+        child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Container(
+                  height: 43,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                        color: const Color.fromRGBO(21, 106, 155, 1)),
+                    //color: const Color.fromRGBO(21, 106, 155, 1),
+                    borderRadius: const BorderRadius.all(Radius.circular(30)),
+                  ),
+                  child: Row(
+                    children: const [
+                      SizedBox(width: 10),
+                      Icon(
+                        Icons.insert_emoticon,
+                        size: 25.0,
+                        color: Color.fromRGBO(21, 106, 155, 1),
+                      ),
+                      SizedBox(width: 8.0),
+                      Expanded(
+                          child: TextField(
+                        textAlignVertical: TextAlignVertical.center,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                        ),
+                      )),
+                      SizedBox(width: 8.0),
+                      Icon(
+                        Icons.image,
+                        size: 25.0,
+                        color: Color.fromRGBO(21, 106, 155, 1),
+                      ),
+                      SizedBox(width: 10.0),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(left: 25),
+                height: 45,
+                width: 45,
+                //color: Colors.white,
+                decoration: const BoxDecoration(
+                  color: Color.fromRGBO(21, 106, 155, 1),
+                  shape: BoxShape.circle,
+                ),
+                child: const IconButton(
+                    onPressed: null,
+                    icon: Icon(
+                      Icons.send_rounded,
+                      color: Colors.white,
+                    )),
+              )
+            ]),
+      ),
+    );
+  }
+}
+
+class ChatingSection extends StatelessWidget {
+  final String senderProfile = 'assets/images/logo.png';
+  final String receiverProfile = 'assets/images/logo.png';
+  const ChatingSection({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      height: double.infinity,
+      color: Colors.white,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 45),
+            TextMessage(
+              message: "Months on ye at by esteem",
+              date: "17:19",
+              senderProfile: senderProfile,
+              isReceiver: 1,
+              isDirect: 0,
+            ),
+            TextMessage(
+              message: "Seen you eyes son show",
+              date: "17:13",
+              senderProfile: senderProfile,
+              isReceiver: 0,
+              isDirect: 0,
+            ),
+            TextMessage(
+              message: "As tolerably recommend shameless",
+              date: "17:10",
+              senderProfile: senderProfile,
+              isReceiver: 0,
+              isDirect: 1,
+            ),
+            TextMessage(
+              message: "She although cheerful perceive",
+              date: "17:10",
+              senderProfile: senderProfile,
+              isReceiver: 1,
+              isDirect: 0,
+            ),
+            const ImageMessage(
+              image: 'assets/images/logo.png',
+              date: "17:09",
+              description: "Least their she you now above going stand forth",
+            ),
+            TextMessage(
+              message: "Provided put unpacked now but bringing. Provided put unpacked now but bringing. Provided put unpacked now but bringing. ",
+              date: "16:59",
+              senderProfile: senderProfile,
+              isReceiver: 1,
+              isDirect: 0,
+            ),
+            TextMessage(
+              message: "Under as seems we me stuff",
+              date: "16:53",
+              senderProfile: senderProfile,
+              isReceiver: 0,
+              isDirect: 0,
+            ),
+            TextMessage(
+              message: "Next it draw in draw much bred",
+              date: "16:50",
+              senderProfile: senderProfile,
+              isReceiver: 0,
+              isDirect: 1,
+            ),
+            TextMessage(
+              message: "Sure that that way gave",
+              date: "16:48",
+              senderProfile: senderProfile,
+              isReceiver: 1,
+              isDirect: 0,
+            ),
+            const SizedBox(height: 15),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TextMessage extends StatelessWidget {
+  final String message, date, senderProfile;
+  final int isReceiver, isDirect;
+
+  const TextMessage({
+    Key? key,
+    required this.message,
+    required this.date,
+    required this.senderProfile,
+    required this.isReceiver,
+    required this.isDirect,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 5),
+      child: Row(
+        children: [
+          isReceiver == 1 && isDirect == 0
+              ? Container(
+                  margin: const EdgeInsets.only(right: 15),
+                  width: 45,
+                  height: 45,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: AssetImage(senderProfile),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                )
+              : SizedBox(
+                  width: 60,
+                  child: Row(
+                    children: [
+                      const Icon(
+                        Icons.check,
+                        color: Color.fromRGBO(21, 106, 155, 1),
+                        size: 13.0,
+                      ),
+                      const SizedBox(width: 7.0),
+                      Text(
+                        date,
+                        style: const TextStyle(
+                          color: Color.fromRGBO(21, 106, 155, 1),
+                          fontSize: 12,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+          Expanded(
+            child: Container(
+              alignment: Alignment.centerLeft,
+              margin: isReceiver == 1
+                  ? const EdgeInsets.only(
+                      right: 25,
+                    )
+                  : const EdgeInsets.only(
+                      left: 20,
+                    ),
+              padding: const EdgeInsets.all(6),
+              height: 55,
+              decoration: isReceiver == 1
+                  ? const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(12),
+                        topRight: Radius.circular(12),
+                        bottomLeft: Radius.circular(12),
+                      ),
+                    )
+                  : const BoxDecoration(
+                      color: Color.fromRGBO(21, 106, 155, 1),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(15),
+                        topRight: Radius.circular(15),
+                        bottomRight: Radius.circular(15),
+                      ),
+                    ),
+              child: Text(
+                message,
+                style: TextStyle(
+                  color: isReceiver == 1
+                      ? const Color.fromRGBO(21, 106, 155, 1)
+                      : Colors.white,
+                  fontSize: 12,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+          ),
+          isReceiver == 1 && isDirect == 0
+              ? SizedBox(
+                  width: 60,
+                  child: Row(
+                    children: [
+                      const Icon(
+                        Icons.check,
+                        color: const Color.fromRGBO(21, 106, 155, 1),
+                        size: 13.0,
+                      ),
+                      const SizedBox(
+                        width: 7.0,
+                      ),
+                      Text(
+                        date,
+                        style: TextStyle(
+                          color: const Color.fromRGBO(21, 106, 155, 1),
+                          fontSize: 12,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              : Container(),
+          isDirect == 0 && isReceiver == 0
+              ? Container(
+                  margin: const EdgeInsets.only(
+                    left: 16,
+                    right: 10,
+                  ),
+                  width: 45,
+                  height: 45,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: AssetImage(senderProfile),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                )
+              : Container(),
+          isReceiver == 0 && isDirect == 1
+              ? Container(
+                  margin: const EdgeInsets.only(
+                    left: 16,
+                    right: 10,
+                  ),
+                  width: 45,
+                  height: 45,
+                )
+              : Container(),
+        ],
+      ),
+    );
+  }
+}
+
+class ImageMessage extends StatelessWidget {
+  final String image, date, description;
+
+  const ImageMessage({
+    Key? key,
+    required this.image,
+    required this.date,
+    required this.description,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          margin: const EdgeInsets.only(
+            right: 16,
+          ),
+          width: 45,
+          height: 45,
+        ),
+        Expanded(
+          child: Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(
+                  right: 26,
+                  top: 5,
+                ),
+                height: 150,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(image),
+                    fit: BoxFit.cover,
+                  ),
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(22.0),
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(
+                  top: 8,
+                  right: 25,
+                  bottom: 10,
+                ),
+                padding: const EdgeInsets.fromLTRB(15, 15, 15, 15),
+                height: 55,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(12)),
+                ),
+                child: Wrap(children: [
+                  Text(
+                    description,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      color: Color.fromRGBO(21, 106, 155, 1),
+                      fontSize: 12,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ]),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(
+          width: 60,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: const [
+              Icon(
+                Icons.check,
+                color: Color.fromRGBO(21, 106, 155, 1),
+                size: 13.0,
+              ),
+              SizedBox(width: 7.0),
+              Text(
+                "17:14",
+                style: TextStyle(
+                  color: Color.fromRGBO(21, 106, 155, 1),
+                  fontSize: 12,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
     );
   }
 }
